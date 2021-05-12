4f7d63dc7df08050c2766f50ace3bd57c059942b
danielppagotto <danielppagotto@gmail.com>
2021-05-12 13:36:31 -0300

    criar arquivos na nuvem

---
+++ Violência e Empreendedorismo/violencia e empreendedorismo.R
@@ -0,0 +1,52 @@
+library(microdatasus); library(tidyverse); library(forcats); library(readxl)
+
+setwd("~/LAPEI/violência e empreendedorismo")
+
+# lendo base --------------------------------------------------------------
+
+#Base SIM-DO 2018
+
+SIM_DO2018 <- fetch_datasus(year_start = 2018, month_start = 1, year_end = 2018,month_end = 12, uf= "GO", 
+                            information_system = "SIM-DO" ) %>% 
+              janitor::clean_names()
+
+cid_10 <- read_excel("~/LAPEI/violência e empreendedorismo/cid_10.xlsx") %>% 
+                            filter(IPEA != "NA") %>% 
+                            janitor::clean_names()
+
+
+### ler a base de projeções populacionais aqui 
+
+populacao <- read_delim("~/LAPEI/violência e empreendedorismo/populacao_residente.csv", 
+                        ";", escape_double = FALSE, locale = locale(encoding = "ISO-8859-1", 
+                                                                    asciify = TRUE), trim_ws = TRUE)
+
+
+populacao_empilhada <- populacao %>% 
+                          gather(key = "sexo", value = "total", 3:4)
+
+populacao_empilhada$ibge <- as.factor(populacao_empilhada$ibge)
+
+# tratamento inicial ------------------------------------------------------
+
+SIM_CONT2018 <- SIM_DO2018 %>%
+  group_by(sexo,codmunocor,causabas)%>%
+  count() %>% 
+  filter(sexo != "0") %>% 
+  left_join(cid_10, by = c("causabas" = "cod_subcat")) %>% 
+  filter(cod_cat != "NA") %>% 
+  select(-subcategoria_2) %>% 
+  mutate(sexo = if_else(sexo == "1", "Masculino", "Feminino")) %>%
+  left_join(populacao_empilhada, by = c("codmunocor"="ibge", "sexo")) %>% 
+  mutate(uf_codigo = str_sub(codmunocor, end = 2)) %>% 
+  filter(uf_codigo == "52")
+
+# Juntar bases de projeções populacional e SIM_CONT2018
+# criar uma nova variável que corresponde à divisão entre o número de óbitos por 
+# sexo e a quantidade de mulheres ou homens 
+
+SIM_CONT2018 %>% 
+  filter(codmunocor == "520870") %>% 
+  ggplot(aes(fct_reorder(ipea,n), n, fill = sexo)) + geom_col() + coord_flip() + 
+  theme_bw() 
+
