aec146dc653f23b331f7b3e6e97133afe1888c7e
admhenri <henriqueribeiro@discente.ufg.br>
2023-06-20 16:20:09 -0300

    Update script - saude do empreendedor.R

--- 07_previsao_saude/previsao_saude/script - saude do empreendedor.R
+++ 07_previsao_saude/previsao_saude/script - saude do empreendedor.R
@@ -12,15 +12,11 @@ base_pns <- pns_2019[["variables"]]
 pns_tratada <- 
   base_pns %>% 
   filter(E01401 == "Conta própria") %>% 
-<<<<<<< HEAD
   select(#identificação e controle
          V0001, V0015, V0026, V0031, 
          #Informações do Domicílio
-         A001, A002010, A003010, A004010, A01001,
-         A011, A005010, A005012, A00601, A009010,
-         A01401, A01402, A01403, A01501, A016010,
-         A018018, A018020, A018022, A018024, A018026,
-         A018028, A01901, A02102, A02201,
+         A001, A005010, A005012, A009010, A01403, 
+         A01501, A016010, A02201,
          #Visitas domiciliares de Equipe de Saúde da Família e Agentes de Endemias 
          B001,
          #Características gerais dos moradores
@@ -29,37 +25,27 @@ pns_tratada <-
          #Características de educação dos moradores
          D00202, D00901, 
          #Características de trabalho das pessoas de 14 anos ou mais de idade
-         E005, E006011, E008, E010010, E010011,
-         E010012, E010013, E011, E01201, E01401,
-         E01501, E01602, E017, E01802, E019,
-         E033,
-         #Rendimentos de outras fontes
-         F001021, F007021, F008021, VDF00102,
+         E005, #E010011, E010012, E010013, 
+         E011, 
+         E01201, E01401, E01501, E01602, E017,
+         E01802, E019, E033,
          #Cobertura de Plano de Saúde
          I00101, I00102,
          #Utilização de serviços de saúde
-         J001, J00101, J002, J003, J00402,
-         J00404, J005, J006, J007, J00801,
-         J009, J01002, J01101, J012, J01301,
-         J014, J037, J038,
+         J00101, #J002, 
+         J012, J01301, J038,
          #Informações para futuros contatos, características do trabalho e apoio social
          M00303, M00401, M00402, M005010, M005011,
-         M00601, M007, M008, M009, M01001,
-         M011011, M011021, M011031, M011041, M011051,
-         M011061, M011071, M01601, M01701, M01801,
-         M01901,
+         M00601, M007, M008, M01601, M01901,
          #Percepção do estado de saúde 
-         N001, N00101, N004, N005, N006,
-         N00701, N010, N011, N012, N013,
-         N014, N015, N016, N017, N018,
+         N004, N005, N006, N00701, N010, 
+         N011, N012, N013, N014, N015, 
+         N016, N017, N018,
          #Acidentes
-         O00101, O00201,
+         O00101,
          #Estilos de vida
-         P00104, P00404, P019, P02002, P02601,
-         P02801, P03301, P034, P035, P03702,
-         P036, P038, P039, P03904, P03906,
-         P04501, P04502, P046, P050, P05404,
-         P05407, P05410, P05413, P05416, P058,
+         P00104, P00404, P019, P02002, P02801, 
+         P03301, P035, P050, P05405,
          #Doenças crônicas
          Q00201, Q03001, Q060, Q06306, Q06307,
          Q06308, Q06309, Q06310, Q068, Q074,
@@ -76,10 +62,8 @@ pns_tratada <-
          H001,
          #Antropometria
          W001, W00103, W00203,
-         #VARIÁVEIS DERIVADAS
-         VDC001, VDC003, VDD004A, VDE001, VDE002,
-         VDE014, VDF002, VDF003, VDF004, VDL001,
-         VDM001)
+         #Variáveis de amostragem
+         V00281)
          
 =======
   select(V0001, )
